22ef497541df3e9a69b31f7c974b5b4d1e950555
REYES_ALFONSO_RAMIREZ_MARTINEZ <a.22308051281289@cbtis128.edu.mx>
2025-02-25 16:59:38 +0000

    Ramirez Imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text(
+              "Mis Imagenes",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              ),
+            ),
+            backgroundColor: Colors.blue),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Reyes Ramirez Mat: 22308051281289',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/ReyesRamirez/Imagenes-para-APP-flutter-6J/refs/heads/main/Sushi1.jpg', // URL de la primera imagen
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Sushi Negro en github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ), // Espacio entre las imágenes
+              const SizedBox(height: 20),
+              Image.network(
+                'https://raw.githubusercontent.com/ReyesRamirez/Imagenes-para-APP-flutter-6J/refs/heads/main/Sushi2.jpeg', // URL de la segunda imagen
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Sushi Variocolor en github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); // Material APP
+  } // Widgets
+} // Clase mi imagen
